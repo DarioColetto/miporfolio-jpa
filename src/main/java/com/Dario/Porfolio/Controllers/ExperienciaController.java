7f6322d9394d00517f240b970669d9aa7af7c5ed
Dario Coletto <darocoletto@gmail.com>
2022-07-28 18:09:34 +0200

    cambio origin

--- src/main/java/com/Dario/Porfolio/Controllers/ExperienciaController.java
+++ src/main/java/com/Dario/Porfolio/Controllers/ExperienciaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge=3600)
+@CrossOrigin(origins = "https://dariocoletto-ac579.web.app", maxAge=3600)
 
 @RestController
 @RequestMapping("/experiencia")
--- src/main/java/com/Dario/Porfolio/Controllers/TecnologiaController.java
+++ src/main/java/com/Dario/Porfolio/Controllers/TecnologiaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge=3600)
+@CrossOrigin(origins = "https://dariocoletto-ac579.web.app", maxAge=3600)
 @RestController
 @RequestMapping("/tecnologia")
 public class TecnologiaController {
